4f128fcf32c83b94b86cb91afdaf4dd39d8a5ac2
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-05-20 03:59:24 +0530

    respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,6 +17,9 @@ public class Main {
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+       clientSocket.getOutputStream().write(
+               "HTTP/1.1 200 OK\r\n\r\n".getBytes()
+       );
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
