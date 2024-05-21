e610270f4d52ced2f920d0c1dff7d115516fad2e
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-05-21 16:09:28 +0530

    concurrency feature added
    now the server can handle multiple requests using multithreading

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,72 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
 
-    // Uncomment this block to pass the first stage
 
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
+        System.out.println("Logs from your program will appear here!");
 
-     try {
-       serverSocket = new ServerSocket(4221);
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-//       clientSocket.getOutputStream().write(
-//               "HTTP/1.1 200 OK\r\n\r\n".getBytes()
-//       );
-         OutputStream outputStream = clientSocket.getOutputStream();
-         InputStream input = clientSocket.getInputStream();
-         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-         String line =reader.readLine();
-         String[] httpRequest = line.split(" ",  0);
-         System.out.println(line);
-         if(httpRequest[1].startsWith("/echo/")){
-             String param = httpRequest[1].substring(6);
-             String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+param.length()+"\r\n\r\n"+param;
-             outputStream.write(response.getBytes());
-         }else if(httpRequest[1].equals("/")){
-             outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-             System.out.println("Accepted new connection and responded.");
-         }else if(httpRequest[1].equals("/user-agent")){
-             reader.readLine();
-             String userAgentValue = reader.readLine().split(" ", 0)[1];
-             String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+userAgentValue.length()+"\r\n\r\n"+userAgentValue;
-             outputStream.write(response.getBytes());
-         }else {
-             outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-             System.out.println("Rejected the bad connection.");
-         }
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+
+
+        try {
+            serverSocket = new ServerSocket(4221);
+            serverSocket.setReuseAddress(true);
+            while(true){
+                clientSocket = serverSocket.accept(); // Wait for connection from client.
+                HttpRequestHandler requestHandler = new HttpRequestHandler(clientSocket);
+                Thread.startVirtualThread(requestHandler::run);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+}
+class HttpRequestHandler{
+    Socket clientSocket;
+    public HttpRequestHandler(Socket clientSocket){
+        this.clientSocket = clientSocket;
+    }
+    public void run(){
+        try{
+            OutputStream outputStream = clientSocket.getOutputStream();
+            InputStream input = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String line =reader.readLine();
+            String[] httpRequest = line.split(" ",  0);
+            System.out.println("[REQUEST] "+line);
+
+            //routing
+            if(httpRequest[1].startsWith("/echo/")){
+                String param = httpRequest[1].substring(6);
+                String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "
+                        +param.length()+"\r\n\r\n"+param;
+                System.out.println("[RESPONSE] "+response);
+                outputStream.write(response.getBytes());
+
+            }else if(httpRequest[1].equals("/")){
+                String responce = "HTTP/1.1 200 OK\r\n\r\n";
+                outputStream.write(responce.getBytes());
+                System.out.println("[RESPONSE] "+ responce);
+
+            }else if(httpRequest[1].equals("/user-agent")){
+                reader.readLine();
+                String userAgentValue = reader.readLine().split(" ", 0)[1];
+                String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "
+                        +userAgentValue.length()+"\r\n\r\n"+userAgentValue;
+                outputStream.write(response.getBytes());
+                System.out.println("[RESPONSE] "+ response);
+
+            }else {
+                outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+                System.out.println("[ERROR] unknown route.");
+            }
+
+        }catch (IOException e){
+            System.out.println("[IOEXCEPTION] "+ e.getMessage());
+        }
+    }
 }
