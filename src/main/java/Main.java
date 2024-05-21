6e6f1a0893d4f28b4576aefe15d807ca7b4b2ef6
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-05-22 01:32:47 +0530

    Content-Encoding header added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -63,10 +63,10 @@ class HttpRequestHandler{
                 }while(!header.equals("Accept-Encoding:"));
                 String response;
                 if(value.equals("gzip")){
-                    response = "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\n\rContent-Type: text/plain\r\nContent-Length: "
+                    response = "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: "
                             +param.length()+"\r\n\r\n"+param;
                 }else {
-                    response = "HTTP/1.1 200 OK\r\nContent-Encoding: invalid-encoding\n\rContent-Type: text/plain\r\nContent-Length: "
+                    response = "HTTP/1.1 200 OK\r\nContent-Encoding: invalid-encoding\r\nContent-Type: text/plain\r\nContent-Length: "
                             +param.length()+"\r\n\r\n"+param;
                 }
                 System.out.println("[RESPONSE] "+response);
